d9510d28396b4eee8840fcd51cb4b57a9f8acb67
AlexeyVorobyov95 <forced1395@gmail.com>
2023-08-07 11:24:59 +0300

    add team project

---
+++ src/service/api/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const instance = axios.create({
+  baseURL: 'https://final-project-node-5vh7.onrender.com',
+});
+
+export const setToken = token => {
+  instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+};
+
+export const deleteToken = () => {
+  instance.defaults.headers.common['Authorization'] = '';
+};
